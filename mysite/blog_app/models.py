d8fbe83dbe805f69eee2a63de33bd89bdd2660d3
lindberg <ad.lindberg@gmail.com>
2024-09-20 08:21:49 +0200

    Post-class

--- mysite/blog_app/models.py
+++ mysite/blog_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+from django.urls import reverse
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    text = models.CharField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True,null=True)
+    
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+    
+    def approve_comment(self):
+        return self.comments.filter(approved_comments=True)
+    
+    def __str__(self) -> str:
+        return self.title
